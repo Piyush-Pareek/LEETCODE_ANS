75dccde78781abe82f05265a9d04ff03f6329518
Piyush-Pareek <piyushpareek207@gmail.com>
2026-03-30 09:04:40 +0530

    Time: 0 ms (100%), Space: 11.7 MB (56.32%) - LeetHub

--- 0942-di-string-match/0942-di-string-match.cpp
+++ 0942-di-string-match/0942-di-string-match.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<int> diStringMatch(string s) {
-        vector<int>ans(s.length()+1);
-        int count = s.length();
-        for(int i=0;i<s.length();i++){
-            if(i==s.length()-1&&s[i]=='D'){ans[i]=count--;ans[i+1] = count--;}
-            else if(s[i]== 'D'){
-                ans[i] = count--;
-            } 
+        int n=s.size();
+      int high=n;
+      int low=0;
+      vector<int >ans;
+      for(int i=0;i<s.size();i++){
+        if(s[i]=='I'){
+       ans.push_back(low);
+       low++;
         }
-        count = 0;
-        for(int i=0;i<s.length();i++){
-            if(i==s.length()-1&&s[i]=='I'){ans[i]=count++;ans[i+1] = count++;}
-             else if(s[i]== 'I'){
-                ans[i] = count++;
-            } 
+        else{
+            ans.push_back(high);
+            high--;
         }
-        return ans;
+      } 
+      ans.push_back(low);
+      return ans; 
     }
 };
