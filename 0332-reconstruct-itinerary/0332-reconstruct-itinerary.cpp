bbb8a8f6be9bc26c3d986b223a004f08f91a0d35
Piyush-Pareek <piyushpareek207@gmail.com>
2026-04-17 22:04:22 +0530

    Time: 59 ms (8.45%), Space: 24.7 MB (5.09%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+vector<string>ans;
+int count = 0;
+void dfs(map<string,vector<string>>&m,string curr){
+    if(m.find(curr) == m.end()) {
+        ans.push_back(curr);
+
+        return ;
+    }   
+        cout<<count++<<" "<<curr<<endl;
+        auto temp = m[curr];
+       while(m[curr].size()){
+            string c = m[curr].back();
+        m[curr].pop_back();
+        dfs(m,c);
+        }
+    
+    ans.push_back(curr);
+}
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        map<string,vector<string>>m1;
+        for(int i=0;i<tickets.size();i++){
+            m1[tickets[i][0]].push_back(tickets[i][1]); 
+        }
+        for(auto&x:m1){
+            sort(x.second.rbegin(),x.second.rend());
+        }
+        dfs(m1,"JFK");
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
