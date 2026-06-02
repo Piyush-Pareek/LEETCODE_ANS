16794570111e0e12d652225a7a2e81da08df5bbe
Piyush Pareek <piyushpareek207@gmail.com>
2026-06-02 17:28:12 +0530

    Time: 0 ms (100%), Space: 17.7 MB (27.46%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
 
- int climb(int n,int pos,vector<int>&vec,vector<int>&dp ){
+//  int climb(int n,int pos,vector<int>&vec,vector<int>&dp ){
 
-        if(pos >= n){
-            return 0;
-        }
-        if(dp[pos]!=-1)return dp[pos];
-    return dp[pos] = vec[pos]+min(climb(n,pos+1,vec,dp), climb(n,pos+2,vec,dp)) ;
-    }
+//         if(pos >= n){
+//             return 0;
+//         }
+//         if(dp[pos]!=-1)return dp[pos];
+//     return dp[pos] = vec[pos]+min(climb(n,pos+1,vec,dp), climb(n,pos+2,vec,dp)) ;
+    // }
    
-    int minCostClimbingStairs(vector<int>& cost) {
-        vector<int>dp(cost.size(),-1);
-    vector<int>dp1(cost.size(),-1);
-   return min(climb(cost.size(),0,cost,dp),climb(cost.size(),1,cost,dp1));
+    int minCostClimbingStairs(vector<int>& nums) {
+        vector<int>dp(nums.size(),-1);
+        int a=0,b=0;
+        int x =0,y=0;
+        for(int i = nums.size()-1;i>0;i--){
+            int temp = nums[i]+min(x,y);
+            y = x;
+            x = temp;
+        }
+        a = x;
+        x=0,y=0;
+        for(int i =nums.size()-1;i>=0;i--){
+            int temp = nums[i]+min(x,y);
+            y=x;
+            x=temp;
+        }
+        b=x;
+        return min(a,b);
+   
     }
 };
