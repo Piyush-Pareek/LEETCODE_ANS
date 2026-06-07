a70a9aa769d2613d4bbd78f446cc87fedd97f975
Piyush Pareek <piyushpareek207@gmail.com>
2026-06-07 20:15:53 +0530

    Time: 105 ms (46.5%), Space: 259.3 MB (13.78%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
-struct PairHash {
-    template <class T1, class T2>
-    std::size_t operator () (const std::pair<T1, T2>& p) const {
-        auto h1 = std::hash<T1>{}(p.first);
-        auto h2 = std::hash<T2>{}(p.second);
-        
-        // Combine the two hashes (using a simple bitwise shift and XOR to minimize collisions)
-        return h1 ^ (h2 + 0x9e3779b9 + (h1 << 6) + (h1 >> 2));
-    }
-};
-    bool solve(vector<int>&stones,int k,unordered_map<pair<int,int>,int,PairHash>&dp,int i,int prev,int temp){
-        if(i>=stones.size()||k<=0||k>temp) return false;
-        cout<<k<<endl;
-        if(dp.find({i,k-1})!=dp.end())return dp[{i,k-1}];
+    bool solve(vector<int>&stones,int k,vector<vector<int>>&dp,int i,int prev){
+        if(i>=stones.size()||k<0) return false;
+        if(dp[i][k]!=-1)return dp[i][k];
         int l = i,r=stones.size()-1;
         int found =-1;
         int target = prev+k;
@@ -30,27 +19,21 @@ struct PairHash {
         if(found == -1)return false;
         if(found == stones.size()-1) return true;
         prev = stones[found];
-        bool a =solve(stones,k,dp,found+1,prev,temp);
-        bool b = solve(stones,k+1,dp,found+1,prev,temp);
-        bool c = solve(stones,k-1,dp,found+1,prev,temp);
+        bool a =solve(stones,k,dp,found+1,prev);
+        bool b = solve(stones,k+1,dp,found+1,prev);
+        bool c = solve(stones,k-1,dp,found+1,prev);
         if(!(a||b||c)){
-            dp[{i,k-1}] = 0;
+            dp[i][k] = 0;
         }
-        else  dp[{i,k-1}] = 1;
+        else  dp[i][k] = true;
         // cout<<"i "<<i<<" v-"<<dp[i][k]<<" k "<<k<<endl;
-         return dp[{i,k-1}];
+         return dp[i][k];
     }
     bool canCross(vector<int>& stones) {
 
-        int min_val = stones[0],max_val = stones[0];
-        for(int i =1;i<stones.size();i++){
-            min_val = min(stones[i],min_val);
-            max_val = max(stones[i],max_val);
-        }
-        int  k = max_val-min_val;
-        // vector<vector<int>>dp(stones.size(),vector<int>(k,-1));
-        unordered_map<pair<int,int>,int,PairHash>dp;
-        // m[0][k] = 0;
-        return solve(stones,1,dp,1,stones[0],k);
+      
+        int  k = stones.size();
+        vector<vector<int>>dp(stones.size(),vector<int>(k+1,-1));
+        return solve(stones,1,dp,1,stones[0]);
     }
 };
