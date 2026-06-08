b863c81e622094b1cbf7bac382a5cca7ed1085ff
Piyush Pareek <piyushpareek207@gmail.com>
2026-06-08 18:44:42 +0530

    Time: 13 ms (53.57%), Space: 25.8 MB (27.16%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int count  =0;
-    int solve(vector<int>&nums, int target,int sum,int i){
+    int solve(vector<int>&nums, int target,int sum,int i,vector<vector<int>>&dp,int sumi){
         if(target == sum&&i==nums.size()){
             return 1;
         }
         if(i>=nums.size()){
             return 0;
         }
-        int a = solve(nums,target,sum+nums[i],i+1);
-        int b = solve(nums,target,sum-nums[i],i+1);
-        return a+b;
+        int x=0;
+        if(sum>=0) x = sum;
+        else{
+            x = abs(sum)+sumi+1;
+        }
+        if(dp[i][x]!=-1) return dp[i][x];
+        int a = solve(nums,target,sum+nums[i],i+1,dp,sumi);
+        int b = solve(nums,target,sum-nums[i],i+1,dp,sumi);
+        return dp[i][x] =  a+b;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(nums,target,0,0);
+        int sum = 0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+        }
+        vector<vector<int>>dp(nums.size(),vector<int>(2*sum+2,-1));
+        return solve(nums,target,0,0,dp,sum);
     }
 };
