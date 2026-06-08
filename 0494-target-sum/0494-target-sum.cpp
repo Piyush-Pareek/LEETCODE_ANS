39cb5b977ac7a11be1dd4ff19d50238305f3c177
Piyush Pareek <piyushpareek207@gmail.com>
2026-06-08 19:15:58 +0530

    Time: 16 ms (47.4%), Space: 26 MB (22.43%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
-    int count  =0;
-    int solve(vector<int>&nums, int target,int sum,int i,vector<vector<int>>&dp,int sumi){
-        if(target == sum&&i==nums.size()){
-            return 1;
-        }
-        if(i>=nums.size()){
-            return 0;
-        }
-        int x=0;
-        if(sum>=0) x = sum;
-        else{
-            x = abs(sum)+sumi+1;
-        }
-        if(dp[i][x]!=-1) return dp[i][x];
-        int a = solve(nums,target,sum+nums[i],i+1,dp,sumi);
-        int b = solve(nums,target,sum-nums[i],i+1,dp,sumi);
-        return dp[i][x] =  a+b;
-    }
+    // int count  =0;
+    // int solve(vector<int>&nums, int target,int sum,int i,vector<vector<int>>&dp,int sumi){
+    //     if(target == sum&&i==nums.size()){
+    //         return 1;
+    //     }
+    //     if(i>=nums.size()){
+    //         return 0;
+    //     }
+    //     int x=0;
+    //    x = sum+sumi;
+    //     if(dp[i][x]!=-1) return dp[i][x];
+    //     int a = solve(nums,target,sum+nums[i],i+1,dp,sumi);
+    //     int b = solve(nums,target,sum-nums[i],i+1,dp,sumi);
+    //     return dp[i][x] =  a+b;
+    // }
     int findTargetSumWays(vector<int>& nums, int target) {
         int sum = 0;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
         }
-        vector<vector<int>>dp(nums.size(),vector<int>(2*sum+2,-1));
-        return solve(nums,target,0,0,dp,sum);
+        if(abs(target)>sum) return 0;
+        vector<vector<int>>dp(nums.size()+1,vector<int>(2*sum+1,0));
+        // return solve(nums,target,0,0,dp,sum);
+        
+        dp[nums.size()][target+sum] =1;
+        for(int i=nums.size()-1;i>=0;i--){
+            for(int j=2*sum;j>=(0);j--){
+
+                int a=0 ,b =0;
+                if(j+nums[i]<=2*sum)
+                a=  dp[i+1][j+nums[i]];
+                if(j-nums[i]>=0)
+                 b = dp[i+1][j-nums[i]];
+                 dp[i][j] = a+b;
+            }
+        }
+        return dp[0][0+sum];
     }
 };
