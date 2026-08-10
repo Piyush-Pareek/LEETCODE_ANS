e2c8de48c705b4ac077f06bb1ccaf592ee90817e
Piyush Pareek <piyushpareek207@gmail.com>
2026-08-10 09:41:48 +0530

    Time: 0 ms (100%), Space: 8.7 MB (64.51%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -14,11 +14,29 @@ int solve(string s,int i,vector<int>&dp){
     return dp[i] = a+b;
 }
     int numDecodings(string s) {
-        vector<int>dp( s.length(),-1);
+        vector<int>dp( s.length()+1);
+        dp[s.length()]=1;
+        if(s[s.length()-1]!='0')
+        dp[s.length()-1]=1;
+        else      dp[s.length()-1]=0;
         if(s[0]=='0')return 0;
         for(int i=0;i<s.length()-1;i++){
             if(s[i+1]=='0'&&(s[i]>'2'||s[i]=='0')) return 0;
         }
-        return solve(s,0,dp);
+        // return solve(s,0,dp);
+        for(int i = s.length()-2;i>=0;i--){
+          if(s[i]>'2'){
+            dp[i]=dp[i+1];
+          }else if(s[i] == '0'){
+                dp[i]=0;
+          }else{
+              if((s[i]=='1'||s[i]=='2')&&(s[i]=='1'||s[i+1]<'7')){
+                dp[i]=dp[i+1]+dp[i+2];
+              }else{
+                dp[i]=dp[i+1];
+              }
+          }
+        }
+    return dp[0];
     }
 };                                                                               
