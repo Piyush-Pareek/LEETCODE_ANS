0e9d3c4aba3adf9e1dbd27f5347e92af9a12a387
Piyush Pareek <piyushpareek207@gmail.com>
2026-08-14 07:11:13 +0530

    Time: 83 ms (15.09%), Space: 54.8 MB (10.62%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -10,7 +10,19 @@ public:
         return dp[curr] =  false;
     }
     bool canJump(vector<int>& nums) {
-        vector<int>dp(nums.size()+1,-1);
-        return solve(nums,0,dp);
+        vector<int>dp(nums.size()+1);
+        dp[nums.size()] = 1;
+        dp[nums.size()-1] = 1;
+        for(int i=nums.size()-1;i>=0;i--){
+            for(int j = i+1;j<=i+nums[i];j++){
+                if(dp[j] == 1){
+                    dp[i] = 1;
+                    break;
+                }
+
+            }
+
+        }
+        return dp[0];
     }
 };
