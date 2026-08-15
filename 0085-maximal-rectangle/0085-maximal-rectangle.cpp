390202d0abc7017ccf85bb31da4438e7fb01d0ef
Piyush Pareek <piyushpareek207@gmail.com>
2026-08-15 08:34:46 +0530

    Time: 30 ms (8.47%), Space: 19.4 MB (61.84%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,58 +1,51 @@
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-        vector<vector<pair<int,int>>>dp(matrix.size(),vector<pair<int,int>>(matrix[0].size(),{0,0}));
-        for(int i = matrix.size()-1;i>=0;i--){
-            for(int j = matrix[0].size()-1;j>=0;j--){
-                if(i == matrix.size()-1&&j == matrix[0].size()-1){
-                    if(matrix[i][j] == '1'){
-                        dp[i][j].first = 1;dp[i][j].second = 1;
+        vector<vector<pair<int, int>>> dp(
+            matrix.size(), vector<pair<int, int>>(matrix[0].size(), {0, 0}));
+        for (int i = matrix.size() - 1; i >= 0; i--) {
+            for (int j = matrix[0].size() - 1; j >= 0; j--) {
+                if (i == matrix.size() - 1 && j == matrix[0].size() - 1) {
+                    if (matrix[i][j] == '1') {
+                        dp[i][j].first = 1;
+                        dp[i][j].second = 1;
                     }
-                }
-                else if(i == matrix.size()-1){
-                     if(matrix[i][j] == '1'){
-                    dp[i][j].first = dp[i][j+1].first+1;
-                    dp[i][j].second = 1;
-                     }
-                }else if(j == matrix[0].size()-1){
-                   
-                     if(matrix[i][j] == '1'){
-                    dp[i][j].first =1;
-                       
-                    dp[i][j].second =  dp[i+1][j].second+1;;
+                } else if (i == matrix.size() - 1) {
+                    if (matrix[i][j] == '1') {
+                        dp[i][j].first = dp[i][j + 1].first + 1;
+                        dp[i][j].second = 1;
                     }
-                }else if(i < matrix.size()-1&&j < matrix[0].size()-1 ){
-                    if(matrix[i][j] == '1'){
-                        dp[i][j].first = dp[i][j+1].first+1;
-                        dp[i][j].second = dp[i+1][j].second+1;
+                } else if (j == matrix[0].size() - 1) {
+
+                    if (matrix[i][j] == '1') {
+                        dp[i][j].first = 1;
+
+                        dp[i][j].second = dp[i + 1][j].second + 1;
+                        ;
+                    }
+                } else if (i < matrix.size() - 1 && j < matrix[0].size() - 1) {
+                    if (matrix[i][j] == '1') {
+                        dp[i][j].first = dp[i][j + 1].first + 1;
+                        dp[i][j].second = dp[i + 1][j].second + 1;
                     }
                 }
             }
         }
-for(int i =0;i<matrix.size();i++){
-    for(int j =0;j<matrix[0].size();j++){
-        cout<<dp[i][j].first<<" "<<dp[i][j].second<<" ,";
-    }
-    cout<<endl;
-}
-int maxRectArea = 0;
-for(int i =0;i<matrix.size();i++){
-    for(int j =0;j<matrix[0].size();j++){
-        int bottom = dp[i][j].second;
-        int right = dp[i][j].first;
-        int maxArea = max(bottom,right);
-        for(int k = i+1;k<i+bottom;k++){
-            int x = min(right,dp[k][j].first);
-            right = x;
-            int area = x*(k-i+1);
-            maxArea = max(maxArea,area);
+        int maxRectArea = 0;
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                int bottom = dp[i][j].second;
+                int right = dp[i][j].first;
+                int maxArea = max(bottom, right);
+                for (int k = i + 1; k < i + bottom; k++) {
+                    int x = min(right, dp[k][j].first);
+                    right = x;
+                    int area = x * (k - i + 1);
+                    maxArea = max(maxArea, area);
+                }
+                maxRectArea = max(maxArea, maxRectArea);
+            }
         }
-        cout<<maxArea<<" ,";
-        maxRectArea = max(maxArea,maxRectArea);
-    }
-    cout<<endl;
-    
-}
-return maxRectArea;
+        return maxRectArea;
     }
 };
